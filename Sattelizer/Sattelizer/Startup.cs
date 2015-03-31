b2ec9471597d38807f6973ff923abbe1df7e6fbb
Charles Peet <charles.peet@learning-circle.org>
2015-03-31 16:30:35 -0400

    Just added a basic MVC site, need to weed out all the crap

---
+++ Sattelizer/Sattelizer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sattelizer.Startup))]
+namespace Sattelizer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
