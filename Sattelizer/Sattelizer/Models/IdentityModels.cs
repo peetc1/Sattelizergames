9cdf333dcbb6648263ac0f150ca759d6eb3f24b4
peetc1 <cpeet@insight.rr.com>
2015-04-03 19:37:06 -0400

    changed over to Sattelizer.Models for db context of user login

--- Sattelizer/Sattelizer/Models/IdentityModels.cs
+++ Sattelizer/Sattelizer/Models/IdentityModels.cs
@@ -20,7 +20,7 @@ namespace Sattelizer.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("SattelizerModel", throwIfV1Schema: false)
         {
         }
 
